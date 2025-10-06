43151b1e174710f8b52ec2460c7b12989ba8a773
AshV <fallencrowngames@gmail.com>
2025-10-06 05:24:19 -0700

    Push to make cloudflare do first build

---
+++ js/index.js
@@ -0,0 +1,21 @@
+﻿/*
+import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
+
+export default {
+    async fetch(request, env, ctx) {
+        const url = new URL(request.url);
+
+        // Redirect "/" → "/420clock.html"
+        if (url.pathname === "/" || url.pathname === "") {
+            url.pathname = "/420clock.html";
+            request = new Request(url, request);
+        }
+
+        try {
+            // Serve files from /public
+            return await getAssetFromKV({ request, env, ctx });
+        } catch (err) {
+            return new Response('Not found', { status: 404 });
+        }
+    },
+};
